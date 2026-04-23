274274143e9c977c75d4351368be61ae65ff5957
kacchi-unity <wodn8698@naver.com>
2026-04-23 15:26:04 +0900

    힌트 시스템 연출 구현 (Fade In/Out, Blink)
    HintManager.cs
    - enum HintRenderMode 추가(힌트 연출 모드) : Fade, Blink 모드 추가
    - ShowHintForSeconds(...) 수정: 힌트 지속 시간이 짧을 경우 Blink 모드로 자동 전환되는 예외 처리 구현 추가
    - Action OnHintStatusCheck 이벤트 <T>: enum HintRenderMode 추가
    - 마이너스 ID 트릭 이용: 데이터 순서 및 무결성 유지 + 페이드아웃 상태 제어 구현
    
    EnemyController.cs
    - 힌트 연출 추가: FadeIn(), FadeOut()
    - Blink 기능 - 기존 isActiveHint() 함수 재사용
    - Action OnPlayerDamaged 이벤트 <T>: float Fade In, Fade Out Duration 추가
    - 이벤트 기반(Action) 통신 구조: HintManager - Enemy Controller 간 디커플링
    
    추후 Fade Out: 깜빡임 기능 추가 가능

--- EnemyController.cs
+++ EnemyController.cs
@@ -1,7 +1,10 @@
-using UnityEngine;
-using TMPro;
+using NUnit.Framework;
 using System;
 using System.Collections;
+using TMPro;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class EnemyController : MonoBehaviour
 {
@@ -11,13 +14,15 @@ public class EnemyController : MonoBehaviour
     ScoreManager ScoreManagerScript;
     public TextMeshProUGUI hintText;
 
-    public static event Action<int> OnPlayerDamaged;
+    public static event Action<int, float, float> OnPlayerDamaged;
 
     public bool isKilledbyPlayer = false;
     bool isMove = true;
     int scoreAmount = 1;
     float moveSpeed = 0.02f;
     float damageToPlayer = 20f;
+    float fadeInDuration = 0.5f;
+    float fadeOutDuration = 0.5f;
 
     private void OnEnable()
     {
@@ -57,7 +62,7 @@ public class EnemyController : MonoBehaviour
             isKilledbyPlayer = false;
             //Debug.Log("플레이어 충돌!");
             int enemyId = this.enemyData.GetId();
-            OnPlayerDamaged?.Invoke(enemyId); //HintManager, etc...
+            OnPlayerDamaged?.Invoke(enemyId, this.fadeInDuration, this.fadeOutDuration); //HintManager, etc...
             enemyListSO.RemoveEnemyData(enemyData);
             Die();
         }
@@ -69,26 +74,83 @@ public class EnemyController : MonoBehaviour
         hintText.text = meaning;
 
         //Check hint status
-        ApplyHint(enemyData.GetId());
+        ApplyHint(enemyData.GetId(), HintManager.HintRenderMode.Blink);
     }
 
-    private void ApplyHint(int targetId)
+    private void ApplyHint(int targetId, HintManager.HintRenderMode mode)
     {
-        if (this.enemyData.GetId() == targetId)
+        //Use absolute values ​​and (-) signs to maintain fade-out and hint hash order.
+
+        if (this.enemyData.GetId() == Mathf.Abs(targetId))
         {
             if (HintStatus.isHintActive(targetId)) //bool type
             {
-                hintText.gameObject.SetActive(true);
+                if (mode == HintManager.HintRenderMode.Fade)
+                {
+                    StartCoroutine(FadeIn());
+                }
+                else if (mode == HintManager.HintRenderMode.Blink)
+                {
+                    hintText.alpha = 1f;
+                    hintText.gameObject.SetActive(true);
+                }
+                
             }
 
             else
             {
-                hintText.gameObject.SetActive(false);
+                if (mode == HintManager.HintRenderMode.Fade)
+                {
+                    Debug.Log("Fade 여기실행");
+                    StartCoroutine(FadeOut());
+                }
+                else if (mode == HintManager.HintRenderMode.Blink)
+                {
+                    hintText.alpha = 0f;
+                    hintText.gameObject.SetActive(false);
+                }
             }
         }
     }
 
-    
+    IEnumerator FadeIn()
+    {
+        if (fadeInDuration == 0f)
+        {
+            fadeInDuration = 0.1f;
+        }
+
+        hintText.gameObject.SetActive(true);
+        float elapsed = 0f;
+        while (elapsed < fadeInDuration)
+        {
+            elapsed += Time.deltaTime;
+            hintText.alpha = Mathf.Lerp(0, 1, elapsed/fadeInDuration);
+            yield return null;
+        }
+        hintText.alpha = 1.0f;
+    }
+
+    IEnumerator FadeOut()
+    {
+        if (fadeOutDuration == 0f)
+        {
+            fadeOutDuration = 0.1f;
+        }
+
+        hintText.alpha = 1f;
+        float elapsed = 0f;
+        while (elapsed < fadeOutDuration)
+        {
+            elapsed += Time.deltaTime;
+            hintText.alpha = Mathf.Lerp(1, 0, elapsed / fadeOutDuration);
+            yield return null;
+        }
+        hintText.alpha = 0f;
+        hintText.gameObject.SetActive(false);
+    }
+
+
 
     void Start()
     {
--- HintManager.cs
+++ HintManager.cs
@@ -8,9 +8,15 @@ using static UnityEngine.GraphicsBuffer;
 
 public class HintManager : MonoBehaviour
 {
-    float hintDurationSeconds = 2f;
+    float hintDurationSeconds = 10f;
 
-    public static event Action<int> OnHintStatusCheck;
+    public static event Action<int, HintRenderMode> OnHintStatusCheck;
+
+    public enum HintRenderMode
+    {
+        Blink,
+        Fade
+    }
 
     private void OnEnable()
     {
@@ -22,26 +28,44 @@ public class HintManager : MonoBehaviour
         EnemyController.OnPlayerDamaged -= ManagementHint;
     }
 
-    void ManagementHint(int targetId)
+    void ManagementHint(int targetId, float fadeInDuration, float fadeOutDuration)
     {
         if (!HintStatus.isHintActive(targetId)) //return bool type
         {
-            StartCoroutine(ShowHintForSeconds(targetId, hintDurationSeconds));
+            HintRenderMode currentHintRenderMode = this.hintDurationSeconds > fadeInDuration + fadeOutDuration
+                ? HintRenderMode.Fade
+                : HintRenderMode.Blink;
+
+            StartCoroutine(ShowHintForSeconds(targetId, fadeOutDuration, currentHintRenderMode));
         }
 
         else { Debug.Log(targetId.ToString() + " 은 이미 힌트 활성화되었습니다."); }
     }
 
-    IEnumerator ShowHintForSeconds(int targetId, float duration)
+    IEnumerator ShowHintForSeconds(int targetId, float fadeOutDuration, HintRenderMode mode)
     {
         Debug.Log(targetId.ToString()+ " 을 해쉬에 등록합니다");
         HintStatus.Add(targetId);
-        OnHintStatusCheck?.Invoke(targetId);
+        OnHintStatusCheck?.Invoke(targetId, mode);
 
-        yield return new WaitForSeconds(duration);
+        yield return new WaitForSeconds(hintDurationSeconds - fadeOutDuration);
 
-        HintStatus.Remove(targetId);
-        OnHintStatusCheck?.Invoke(targetId);
+        if (mode == HintRenderMode.Fade)
+        {
+            OnHintStatusCheck?.Invoke(-targetId, mode); //Using a (-) sign for only apply fading effect, Trick the internal function
+            yield return new WaitForSeconds(fadeOutDuration);
+
+            HintStatus.Remove(targetId); //Code where the Hint Word ID actually disappears
+        }
+
+        else if (mode == HintRenderMode.Blink)
+        {
+            yield return new WaitForSeconds(fadeOutDuration);
+
+            HintStatus.Remove(targetId);
+            OnHintStatusCheck?.Invoke(targetId, mode);
+        }
+        
         Debug.Log(targetId.ToString() + "의 힌트가 사라집니다.");
     }
 }
