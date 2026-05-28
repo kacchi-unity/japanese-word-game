653a4ece58dc42d2744ab648efab3deb3874b6ff
kacchi-unity <wodn8698@naver.com>
2026-05-28 17:00:56 +0900

    refactor: 기존 Alert Modal 스크롤 뷰 기능 추가, 자료 출처 버튼 추가
    UI_Modal_Alert.prefab 구조 수정
    - 기존 Modal; Alert 프리펩 내 ViewPort 및 SliderBar 추가
    - ViewPort 크기를 참고하여 SliderBar 자동 크기 조절 및 숨김 처리
    - ViewPort 내: 기존의 Message Text Mesh Pro를 자식 오브젝트로 경로 설정
    * 알림창 프리펩을 재사용하고자 자동 크기 조절 슬라이더 바와 스크롤뷰 컴포넌트를 추가함.
    
    TitleButtonManager.cs 수정
    - string creaditText 선언
    : [TextArea(10, 20)] 속성 부여 - 인스펙터 Text area 추가
    : 해당 인스펙터 텍스트 입력 창 내 자료 출처, 링크, 에셋 스토어 링크, 골드메탈과 프로젝트 정보를 간단하게 추가함.
    
    - Button sourceCreidtButton 선언
    : 직렬화 버튼 선언 및 연결
    : OnEnable, OnDisable 내 Listener 관리 (추가 및 해제)
    
    -PrintSourceCredit
    : creaditText를 message 매개변수로 하여 기존 ShowAlertModal() 메서드 호출 -> 알림창 재사용, 스크롤바 가능
    
    Scene_Title
    - Source Credit Button 추가

--- TitleButtonManager.cs
+++ TitleButtonManager.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.AddressableAssets;
 
 public class TitleButtonManager : MonoBehaviour
 {
+    [Header ("Button Connect")]
+    [Tooltip ("타이틀 씬 하단 버튼을 연결하세요.")]
     [SerializeField] private Button startButton;
+    [SerializeField] private Button sourceCreidtButton;
+
+    [Header("Credits Data")]
+    [Tooltip("크레딧 텍스트를 입력하세요.")]
+    [TextArea(10, 20)]
+    [SerializeField] private string creaditText;
 
     private void OnEnable()
     {
         if (startButton != null)
         {
             startButton.onClick.AddListener(OnClickStartButton);
+            sourceCreidtButton.onClick.AddListener(PrintSourceCredit);
         }
     }
 
@@ -20,11 +27,18 @@ public class TitleButtonManager : MonoBehaviour
         if (startButton != null)
         {
             startButton.onClick.RemoveListener(OnClickStartButton);
+            sourceCreidtButton.onClick.RemoveListener(PrintSourceCredit);
         }
     }
 
     public void OnClickStartButton()
     {
         BaseSceneManager.Instance.ChangeSceneWithLoading(CoreSceneType.Scene_Lobby);
+    }
+
+    public void PrintSourceCredit()
+    {
+        string smallText = $"<size=63>{creaditText}</size>";
+        ModalManager.Instance.ShowAlertModal(smallText, null);
     }
 }
